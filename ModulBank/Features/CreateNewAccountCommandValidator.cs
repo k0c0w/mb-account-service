e10e076b97c3eca780c4cadf2369d7e44ac6b101
Artur Yakupov <art.kazan@mail.ru>
2025-07-28 11:30:42 +0300

    added validation of interest of rate

--- ModulBank/Features/CreateNewAccountCommandValidator.cs
+++ ModulBank/Features/CreateNewAccountCommandValidator.cs
@@ -17,5 +17,14 @@ public class CreateNewAccountCommandValidator : AbstractValidator<CreateNewAccou
         
         RuleFor(x => x.CurrencyCode)
             .SetValidator(new CurrencyCodeValidator());
+
+        When(x => x.AccountType?.ToLower() != "checking", () =>
+        {
+            RuleFor(x => x.InterestRate)
+                .NotNull()
+                .NotEmpty()
+                .LessThanOrEqualTo(3m)
+                .GreaterThanOrEqualTo(-3m);
+        });
     }
 }
