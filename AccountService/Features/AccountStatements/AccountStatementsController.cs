5eb7f4b8545a170228397f4d103ff8ca1d5b2d74
Artur Yakupov <art.kazan@mail.ru>
2025-08-01 11:23:53 +0300

    added forgotten controller

---
+++ AccountService/Features/AccountStatements/AccountStatementsController.cs
@@ -0,0 +1,22 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountService.Features.AccountStatements;
+
+[Route("account-statements")]
+public class AccountStatementsController(IMediator mediator)
+    : ControllerBase
+{
+    [HttpGet("")]
+    public async Task<IResult> GetStatementForUserAsync([FromQuery] Guid accountId,
+        [FromQuery] DateTimeOffset periodStartUtc,
+        [FromQuery] DateTimeOffset? periodEndUtc
+        )
+    {
+        periodEndUtc ??= DateTimeOffset.UtcNow;
+
+        var statement = await mediator.Send(new GetAccountStatementQuery(accountId, periodStartUtc, periodEndUtc.Value));
+
+        return Results.Ok(statement);
+    }
+}
