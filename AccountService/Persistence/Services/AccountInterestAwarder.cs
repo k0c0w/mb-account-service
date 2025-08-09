c273ea46380d817279a81ed9b02b840164f65f08
Artur Yakupov <art.kazan@mail.ru>
2025-08-09 22:26:01 +0300

    fixed accrual: serializable isolation and correct procedure name

--- AccountService/Persistence/Services/AccountInterestAwarder.cs
+++ AccountService/Persistence/Services/AccountInterestAwarder.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using AccountService.Domain;
 using AccountService.Persistence.DataAccess;
 using Microsoft.EntityFrameworkCore;
@@ -8,10 +9,11 @@ public class AccountInterestAwarder(AccountServiceDbContext dbContext) : IAccoun
 {
     public async Task AccrueInterestAsync(Guid accountId, CancellationToken ct = default)
     {
-        var transaction = await dbContext.Database.BeginTransactionAsync(ct);
+        await using var transaction = await dbContext.Database.BeginTransactionAsync(IsolationLevel.Serializable, ct);
         try
         {
-            await dbContext.Database.ExecuteSqlRawAsync("CALL AccrueInterest(@accountId);", accountId, ct);
+            await dbContext.Database.ExecuteSqlRawAsync("CALL accrue_interest(@accountId);", accountId, ct);    
+            await transaction.CommitAsync(ct);
         }
         catch (Exception)
         {
